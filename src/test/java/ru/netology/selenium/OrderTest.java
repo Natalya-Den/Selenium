1147a9cf293c23676e24b666ef8b2123e9496eb1
Natalya Denisenko <ionova.natalya@mail.ru>
2025-01-29 15:15:34 +0300

    Added tests for tasks 1 and 2

---
+++ src/test/java/ru/netology/selenium/OrderTest.java
@@ -0,0 +1,56 @@
+package ru.netology.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class OrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        //options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void sendFormSuccessfulTest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Василий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79111234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+    }
+
+    @Test
+    void sendFormFailedTest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов_Василий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79111234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+    }
+}
